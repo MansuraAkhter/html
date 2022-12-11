35a563921152ef2ede0f575e5e125bf18f205910
MansuraAkhter <mansura3330@gmail.com>
2022-12-12 00:56:22 +0600

    initial commit

---
+++ sandbox.js
@@ -0,0 +1,21 @@
+alert("hello world");
+
+// string concatination
+
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullname = firstName + ' ' + lastName;
+console.log(fullname);
+
+const title = 'Best reads of 2019';
+const author = 'Mario';
+const likes = '30';
+
+let result =`The blog called ${title} by ${author} has ${likes} likes`;
+console.log(result);
+
+const great = () => 'hello,world';
+
+const bib = great();
+console.log(bib);
